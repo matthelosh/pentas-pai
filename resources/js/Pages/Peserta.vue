234bf7bd529e65068adaf24fd93a47782da64f43
Muhammad Soleh <matthelosh@gmail.com>
2023-05-10 15:55:03 +0700

    OnPrecess TWeak Peserta Page

--- resources/js/Pages/Peserta.vue
+++ resources/js/Pages/Peserta.vue
@@ -7,7 +7,7 @@ import _ from 'lodash';
 import Pagination from '@/Components/General/Pagination.vue';
 
 const page = usePage()
-
+const mode = ref('list')
 const parseImg = (url) => {
     let splited = url.split('=')
     if (splited.length > 0) {
@@ -18,6 +18,14 @@ const parseImg = (url) => {
 }
 
 const search = ref('')
+
+const selectedPeserta = ref({})
+
+const detail = (siswa) => {
+    selectedPeserta.value = siswa
+    mode.value = 'detail'
+    console.log(selectedPeserta.value)
+}
 
 watch(search, (value) => {
     axios.get("/peserta?filter="+value, {
@@ -35,7 +43,8 @@ watch(search, (value) => {
 <template>
 <Head title="Data Peserta" />
 <Front>
-    <div class="bg-white w-full p-2 md:p-5 rounded shadow overflow-x-scroll">
+<TransitionGroup type="slide">
+    <div class="bg-white w-full p-2 md:p-5 rounded shadow overflow-x-scroll" key="list" v-if="mode == 'list'">
         <div class="flex justify-end">
             <form action="/peserta" method="get">
                 <input type="text" placeholder="Cari" class="rounded shadow print:hidden;" name="filter" v-model="search
@@ -62,9 +71,9 @@ watch(search, (value) => {
                     <td class="border px-2 py-2 text-center">{{ p+1 }}</td>
                     <td class="border px-2 py-2">{{peserta.nisn}}</td>
                     <td class="border px-2 py-2 flex items-center">
-                        <img :src="parseImg(peserta.foto)" alt="Foto" class="rounded-full aspect-square w-10 shadow object-cover mr-1">
+                        <img :src="parseImg(peserta.foto)" alt="Foto" class="rounded-full aspect-square w-10 shadow object-cover mr-1 cursor-pointer" @click="detail(peserta)" />
                         <!-- {{ peserta.foto }} -->
-                        {{peserta.nama}}
+                        <span @click="detail(peserta)" class="cursor-pointer">{{peserta.nama}}</span>
                     </td>
                     <td class="border px-2 py-2">{{ peserta.jk }}</td>
                     <td class="border px-2 py-2">{{peserta.sekolah.nama}}</td>
@@ -75,5 +84,21 @@ watch(search, (value) => {
         </table>
         <Pagination class="mt-6" :links="page.props.pesertas.links" />
     </div>
+    <div v-if="mode == 'detail'" key="detail" class="w-full">
+        <div class="w-full flex justify-end my-5 px-10">
+            <button class="py-2 px-3 bg-white rounded shadow" @click="mode = 'list'">Tutup</button>
+        </div>
+        <div class="md:mx-10 bg-white shadow rounded-lg p-8 flex  flex-wrap gap-5">
+            <img :src="parseImg(selectedPeserta.foto)" alt="Foto" class="rounded flex-grow md:flex-grow-0 border border-gray-300 w-64 shadow object-cover mr-1" />
+            <div class="bg-[#fefefe] shadow flex-grow p-5 md:p-10">
+                <h1>NAMA: {{ selectedPeserta.nama }}</h1>
+                <h1>NISN: {{ selectedPeserta.nisn }}</h1>
+                <h1>ASAL SEKOLAH: {{ selectedPeserta.sekolah.nama }}</h1>
+                <h1>LOMBA: {{ selectedPeserta.lomba_id }}</h1>
+            </div>
+        </div>
+        
+    </div>
+    </TransitionGroup>
 </Front>
 </template>
