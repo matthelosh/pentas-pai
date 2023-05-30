ec1ec6c24c0b123d3afc7528398b0557d5601187
Muhammad Soleh <matthelosh@gmail.com>
2023-05-30 11:56:26 +0700

    Tes Update Foto

--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -166,6 +166,7 @@ class PesertaController extends Controller
             if ( $request->file('foto')) {
                 $foto = $request->file('foto');
                 $file = Storage::putFileAs('public/img/peserta', $foto, $data->nisn.'.jpg');
+                dd($foto);
             }
             $bidangs = [];
             foreach($data->lomba_id as $kode) {
