acfb0639052ff0d2f1883ea38cdb222bf4449350
matthelosh <matthelosh@gmail.com>
2024-12-15 11:40:02 +0700

    Nomor dada lomba regu

--- resources/js/Components/Peserta/KartuPeserta.vue
+++ resources/js/Components/Peserta/KartuPeserta.vue
@@ -286,82 +286,74 @@ watchEffect(async () => {
                             <div
                                 class="w-full grid grid-cols-3 gap-1 print:gap-2 print:p-0 p-3 relative"
                             >
-                                <div
+                                <!-- <div
                                     v-for="(peserta, p) in group.data"
                                     :key="p"
                                     class="border-gray-800 border shadow print:shadow-none print:break-inside-avoid print:my-1 print:mx-0 mx-auto grid grid-cols-4 relative bg-[url('/img/kartupeserta-bg.png')] bg-cover w-full h-[500px] print:h-[350px]"
+                                > -->
+                                <div
+                                    class="border-gray-800 border shadow print:shadow-none print:break-inside-avoid print:my-1 print:mx-0 mx-auto grid grid-cols-4 relative bg-[url('/img/kartupeserta-bg.png')] bg-cover w-full h-[290mm] print:h-[270mm] w-[206mm]"
                                 >
                                     <div
                                         class="left h-full relative flex flex-col justify-between p-4 items-center bg-opacity-90"
                                         :class="sideBg(props.bidang.kode)"
                                     >
                                         <h1
-                                            class="text-uppercase whitespace-nowrap uppercase font-extrabold w-[100%] text-4xl print:text-2xl text-white"
+                                            class="text-uppercase whitespace-nowrap uppercase font-extrabold w-[100%] text-8xl print:text-8xl text-white ml-14 mt-8"
                                             style="writing-mode: vertical-lr"
                                         >
                                             {{ label }}
                                         </h1>
-                                        <h1
-                                            class="text-4xl bg-white aspect-square print:w-12 w-20 text-center rounded-full flex justify-center items-center"
-                                        >
-                                            {{
-                                                peserta.urutans.length < 1
-                                                    ? group.no
-                                                    : peserta.urutans[0].ke
-                                            }}
-                                        </h1>
                                     </div>
                                     <div class="col-span-3">
                                         <div
-                                            class="logo flex gap-1 items-end justify-center mt-6"
+                                            class="logo text-center gap-1 items-end justify-center mt-14 w-[80%] mx-auto"
                                         >
                                             <img
-                                                src="/img/kkg.png"
+                                                src="/img/logo_pentas.png"
                                                 alt="Logo KKG"
-                                                class="h-10"
+                                                class="h-28 mx-auto"
                                             />
                                             <div class="logo--text">
                                                 <h3
-                                                    class="my-0 leading-4 font-bold text-sm"
+                                                    class="my-0 font-black uppercase text-3xl"
                                                 >
-                                                    PENTAS PAIS 2023
+                                                    {{ page.props.lomba.label }}
                                                 </h3>
-                                                <h2
-                                                    class="my-0 leading-4 text-xs"
-                                                >
-                                                    KKG PAI KECAMATAN WAGIR
-                                                </h2>
                                             </div>
                                         </div>
-                                        <div
-                                            class="foto w-full mt-16 print:mt-2"
+
+                                        <h1
+                                            class="text-[10rem] mx-auto w-[50%] border border-8 my-28 border-black text-center rounded-full"
                                         >
-                                            <img
-                                                :src="imgUrl(peserta.foto)"
-                                                alt="Foto"
-                                                class="aspect-square object-cover rounded-full w-[100px] print:w-[75px] mx-auto object-top border"
-                                            />
-                                        </div>
+                                            {{
+                                                group.data[0].urutans.length < 1
+                                                    ? group.no
+                                                    : group.data[0].urutans[0]
+                                                          .ke
+                                            }}
+                                        </h1>
+
                                         <div
-                                            class="identitas w-full px-2 mt-10 print:mt-2"
+                                            class="identitas w-full px-2 mt-20 print:mt-2"
                                         >
                                             <h2
-                                                class="uppercase text-center leading-4 font-bold text-sm"
+                                                class="uppercase text-center font-bold text-4xl"
                                             >
-                                                {{ peserta.nama }}
+                                                {{ group.data[0].sekolah.nama }}
                                             </h2>
-                                            <h3
-                                                class="text-center leading-4 text-sm"
-                                            >
-                                                No. {{ peserta.nisn }}
+                                            <h3 class="text-center text-xl">
+                                                NPSN.
+                                                {{ group.data[0].sekolah.npsn }}
                                             </h3>
-                                            <h4
+
+                                            <!-- <h4
                                                 class="uppercase text-center leading-4 mt-4 font-bold text-sm"
                                             >
                                                 {{ peserta.sekolah.nama }}
-                                            </h4>
+                                            </h4> -->
                                         </div>
-                                        <vue-qrcode
+                                        <!-- <vue-qrcode
                                             :value="`peserta?nisn=${peserta.nisn}`"
                                             :options="{
                                                 width: 75,
@@ -371,7 +363,7 @@ watchEffect(async () => {
                                                 },
                                             }"
                                             class="absolute bottom-2 left-28"
-                                        ></vue-qrcode>
+                                        ></vue-qrcode> -->
                                     </div>
                                 </div>
                             </div>
